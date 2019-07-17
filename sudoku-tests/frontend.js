6f96897b70167984cb18b1231a3291008c0f07a9
reagankan <rkan3@gatech.edu>
2019-07-16 21:48:26 -0400

    flow control and control checks happen here.

--- sudoku-tests/frontend.js
+++ sudoku-tests/frontend.js
@@ -1,16 +1,44 @@
 var canvas = document.getElementById('canvas');
+var form = document.getElementById('form');
+var highlight = document.getElementById("highlight");
+var verify = document.getElementById("verify");
+
+var mouseAtDefault = true;
 
 var puzzlePair = new PuzzlePair();
 var board; 
 function setup_board() {
     board = new Board(canvas, puzzlePair);
     canvas.addEventListener("click", function(e) {
-        board.clickHandler(e.clientX-canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        mouseAtDefault = false;
+        board.setMouse(e.clientX-canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        if (!board.permanent()) {
+            allowInput();
+        }
+        alert("highlight: " + highlight.checked)
     });
     board.render();
 }
+form.addEventListener("submit", function(e) {
+  e.preventDefault()
+  let input = document.getElementById("number").value
+  alert("submission: " + input);
+  board.writeWithPencil(input);
+  alert("wrote with pencil");
+  disallowInput()
+})
+function allowInput(){
+  form.innerHTML = `<input id="number" type="text" autofocus="autofocus" name="number" value="" maxlength="1" pattern="[1-9]"><br> <input type="submit" value="Submit"> `
+}
+function disallowInput(){
+  form.innerHTML = ""
+}
+
 function main() {
     board.render();
+    if (!mouseAtDefault && highlight.checked) {
+        board.renderHighlight();
+    }
 }
 window.onload = setup_board;
 window.setInterval(main, 1000);
