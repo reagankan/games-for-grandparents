b0e3923b2c8ee3dbc46ccf5a28a3b7b794a94d48
reagankan <rkan3@gatech.edu>
2020-05-14 22:36:17 -0700

    pix<->coor, mv canvas dims, draft Track class.

--- pacman/gridworld.js
+++ pacman/gridworld.js
@@ -32,6 +32,54 @@ IDEA:
 // 	}
 // }
 
-PIX_PER_TILE = 20;
+//bijection: pix(x, y) <--> coor(r, c)
+PIX_PER_TILE = 21;
 ROWS = 31;
 COLS = 28;
+
+W = COLS * PIX_PER_TILE;
+H = ROWS * PIX_PER_TILE;
+function boxIndex(x, y) {
+	return (COLS+1) * Math.floor(y / PIX_PER_TILE) + Math.floor(x / PIX_PER_TILE);
+}
+function pix2Coor(x, y) {
+	var bi = boxIndex(x, y);
+	var r = Math.floor(bi / (COLS+1));
+	var c = bi % (COLS+1);
+	return [r, c];
+}
+function coor2Pix(r, c) {
+	var lowX = c * PIX_PER_TILE;
+	var lowY = r * PIX_PER_TILE;
+
+	var X = lowX + PIX_PER_TILE/2;
+	var Y = lowY + PIX_PER_TILE/2;
+
+	var hiX = lowX + PIX_PER_TILE - 1;
+	var hiY = lowY + PIX_PER_TILE - 1;
+	return [[lowX, X, hiX], [lowY, Y, hiY]];
+}
+
+
+STARTR = 16;
+STARTC = 14;
+
+const base = {
+	ROW : "row",
+	COL : "col"
+}
+var test_exits = new Map([
+	[[STARTR, STARTC - 10], "Left Exit"],
+	[[STARTR, STARTC + 10], "Right Exit"]
+	]);
+class Track {
+	//w.r.t Coordinates, i.e. R, C pairs.
+	constructor(base=base.ROW, base_value=STARTR, exits=test_exits) {
+		this.base = base;
+		this.val = base_value;
+		this.exits = exits;
+	}
+}
+var test_track = new Track();
+
+
